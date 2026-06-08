6ff416a77c8bd1074636304aff1f0a6c44244a84
Josh Yuasa <joshyuasa@gmail.com>
2026-06-08 21:52:33 +0200

    Initialize chess board and implement piece movement

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+import chess
+
+def init_board():
+    global board
+    board = chess.Board()
+    print(board)
+
+def move_piece(move):
+    board.push_san(move)
+    print(board)
+
 def main():
-    print("Hello from pychess!")
+    init_board()
+    move_piece('e4')
 
+    move_piece('e5')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
